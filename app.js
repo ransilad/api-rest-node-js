f680f503674dd14229cd47c57290dc1616500caf
Abel Rangel <ransilad@gmail.com>
2016-06-08 22:13:20 -0400

    Agregando los end-points para administrar el modelo tvshows

--- app.js
+++ app.js
@@ -8,19 +8,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());  
 app.use(methodOverride());
 
-var router = express.Router();
-
-router.get('/', function(req, res) {  
-   res.send("Hello World!");
+mongoose.connect('mongodb://MacBook-Gerson.local:27017/tvshow', function(err, res) {  
+	if(err) {
+		console.log('ERROR: connecting to Database. ' + err);
+	}
 });
+require("./models/tvshow");
 
-app.use(router);
+var TVShowCtrl = require('./controllers/tvshows');
 
-mongoose.connect('mongodb://localhost/tvshows', function(err, res) {  
-  if(err) {
-    console.log('ERROR: connecting to Database. ' + err);
-  }
-  app.listen(3000, function() {
-    console.log("Node server running on http://localhost:3000");
-  });
+// API routes
+var tvshows = express.Router();
+
+tvshows.route('/tvshows')  
+  .get(TVShowCtrl.findAllTVShows)
+  .post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows/:id')  
+  .get(TVShowCtrl.findById)
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
+
+app.use('/api', tvshows);
+
+app.listen(3000, function() {
+console.log("Node server running on http://localhost:3000");
 });
